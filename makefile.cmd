7d969caf363a92ca3fe97cfbcf1cb8c6c8fea329
leo-rp <leorock1988@hotmail.com>
2019-10-26 11:43:03 -0500

    cleaned to be published

--- makefile.cmd
+++ makefile.cmd
@@ -1,5 +1,6 @@
 c:\gbdk\bin\lcc  -Wa-l -c -o main.o main.c
-c:\gbdk\bin\lcc  -Wl-m -Wl-yt1 -o output.gb main.o
+c:\gbdk\bin\lcc  -Wl-m -Wl-yt1 -o parallax.gb main.o
+del *.lst *.map *.sym *.o
 	
 	
 
